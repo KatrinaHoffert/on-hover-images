3df1e5e525c97ea3496591bb3108a0ccc4996a9c
Kat Hoffert <kat.hoffert@gmail.com>
2014-07-23 21:13:06 -0600

    Hovering over images throws alert now

--- data/image-hover.js
+++ data/image-hover.js
@@ -1,1 +1,26 @@
+var imageExtensions = "jpg|jpeg|gif|png|svg";
+
 console.log("Addon is working correctly, page is " + window.location.href);
+
+function init()
+{
+	var allAnchors = document.getElementsByTagName("a");
+
+	for(var i = 0; i < allAnchors.length; i++)
+	{
+		var anchorWithImage = allAnchors[i];
+
+		var matcher = new RegExp(".*\\.(" + imageExtensions + ")$");
+		if(matcher.test(anchorWithImage.href))
+		{
+			anchorWithImage.addEventListener("mouseover", mouseOver);
+		}
+	}
+}
+
+function mouseOver(event)
+{
+	alert(event.target.href);
+}
+
+init();
